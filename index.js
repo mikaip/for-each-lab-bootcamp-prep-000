05a780f5e526020c3641c9757ae8c8015e6cd571
Pia Kim <pkim2320@gmail.com>
2016-09-16 21:58:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+function iterativeLog(array){
+  array.forEach((element, index, array) => {
+    debugger
+    console.log(`${index}: ${element}`)
+  })
+}
 
 function iterate(callback) {
-  var array = []
+  var array = [123]
+  array.forEach(callback)
+  return array
+}
+
+
+function doToArray(array, callback) {
   array.forEach(callback)
 }
